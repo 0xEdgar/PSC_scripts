a4d43d43579e77f8edd4c0147bafad875ec5c9a3
edgarwest <esx@andrew.cmu.edu>
2018-03-18 17:13:11 -0400

    add chestx script

--- job.sh
+++ job.sh
@@ -3,5 +3,5 @@
 JOBNAME="job"
 OUTFN="/pylon5/ac5616p/edgarxi/out-logs/job.log"
 ERRFN="/pylon5/ac5616p/edgarxi/err-logs/job.log"
-sbatch -o $OUTFN -e $ERRFN --job-name $JOBNAME /home/edgarxi/slurmLauncher_GPU.sh python /home/edgarxi/sample_python_file.py
+sbatch -o $OUTFN -e $ERRFN --job-name $JOBNAME /home/edgarxi/slurmLauncher_GPU.sh python /pylon5/ac5616p/edgarxi/chestx/chexnet/Main.py
 
--- slurmLauncher_GPU.sh
+++ slurmLauncher_GPU.sh
@@ -2,13 +2,12 @@
 #SBATCH -p GPU
 #SBATCH -N 1
 #SBATCH -A ac5616p 
-#SBATCH --ntasks-per-node 28
-#SBATCH -t 48:00:00 # HH:MM:SS
+#SBATCH --ntasks-per-node 2
+#SBATCH -t 24:00:00 # HH:MM:SS
 #SBATCH --gres=gpu:p100:2
 #SBATCH --time-min=00:30:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=edgarxi
 
 # echo commands to stdout
-echo "$@"
-"$@"
+set -x
