0c13df5e18daef14fd5f7ff4156373892029de55
Edgar Xi <esx@andrew.cmu.edu>
2018-03-18 20:18:00 -0400

    various bugfixes

--- job.sh
+++ job.sh
@@ -3,5 +3,5 @@
 JOBNAME="job"
 OUTFN="/pylon5/ac5616p/edgarxi/out-logs/job.log"
 ERRFN="/pylon5/ac5616p/edgarxi/err-logs/job.log"
-sbatch -o $OUTFN -e $ERRFN --job-name $JOBNAME /home/edgarxi/slurmLauncher_GPU.sh python /pylon5/ac5616p/edgarxi/chestx/chexnet/Main.py
+sbatch -o $OUTFN -e $ERRFN --job-name $JOBNAME /pylon5/ac5616p/edgarxi/PSC_scripts/slurmLauncher_GPU.sh python /pylon5/ac5616p/edgarxi/chestx/chexnet/Main.py
 
